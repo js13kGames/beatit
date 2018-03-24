ee747f77ce1d6659ceb0446442be456ed419f485
hek4te <hek4te@gmail.com>
2018-03-24 22:32:43 +0100

    added resources and todo list

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,60 @@
 var canvas = document.getElementById('game');
+canvas.style.backgroundColor = 'antiquewhite';
 var ctx = canvas.getContext('2d');
 var mainHero = new Image();
-mainHero.addEventListener('load',drawSetup, false);
-mainHero.src = 'hero.png';
-var enemiesNumber = 10;
+mainHero.addEventListener('load', setup, false);
+mainHero.src = 'hero_sprite.png';
+var intervalRef, 
+	enemiesNumber = 10,
+	heroSliceX = 0,
+	heroSliceY = 0,
+	heroPositionX = 522,
+	heroPositionY = 650;
 
-function drawSetup() {
-	ctx.drawImage(mainHero, 522,650);
-	createEnemies(enemiesNumber);
+function setup() {
+	/*
+		TODO:
+		- licznik punktów (+ zapis w localstorage)
+		- ekran startowy gry (+ komiks przedstawiający fabułę)
+	*/
+
+	drawHero();
+	startHeroAnimation();
+	drawEnemies(enemiesNumber);
 }
 
-function createEnemies(enemiesNumber) {
-	//Make the enemies
+function drawHero(){
+	/*
+		TODO: 
+		- akcje bohatera:
+		-- kliknięcie w obszar na mapie powoduje przemieszczenie się głowy bohatera w kliknięte miejsce. Jeśli głowa postaci i wróg znajdą się w tym samym czasie w miejscu kliknięcia, wtedy dostaje się punkty.
+		- poziom bohatera. Poziom zwiększa się na podstawie punktów. Im większy poziom tym większy: 
+		-- zasięg postaci - jak daleko może chwycić wrogów (zaznaczyć na mapie okręgiem)
+		-- szybkość postaci - jak szybko głowa postaci dosięgnie 
+		- możliwość śmierci bohatera - roślina usycha. Roślina potrzebuję jedzenia żeby nie uschnąć. W miarę zwiększenia poziomu roślina potrzebuje więcej jedzenia i szybciej usycha.
+		- poziom życia bohatera (poziom wyschnięcia rośliny) - jako symboliczna "doniczka" w której zmniejsza się np. poziom wody.
+	*/
+
+	ctx.clearRect(515,650,100,100);
+
+	ctx.drawImage(mainHero, heroSliceX, heroSliceY, 76, 100, heroPositionX, heroPositionY, 76,100);
+
+	heroSliceX+=76;
+	heroPositionX = 515;
+	if(heroSliceX>=mainHero.width) {
+		heroSliceX = 0;
+		heroPositionX = 522;
+	}
+}
+
+function drawEnemies(enemiesNumber) {
+	/*
+		TODO:
+		- przeciwnicy nadlatują z lewej lub prawej, na losowej wysokości z różną prędkością
+		- grafika dla przeciwników
+		- dwa rodzaje przeciwników: mięsne i warzywne. Mięsne dodają punktów i życia, warzywne odbierają życie.
+	*/
+
 	var spacing = 48,
 		xOffset = 150,
 		speed = 2,
@@ -35,6 +78,10 @@ function createEnemies(enemiesNumber) {
 		ctx.fillStyle = '#a1a1a1';
 		ctx.fillRect(posX, posY, 15, 15);
 	}
+}
+
+function startHeroAnimation() {
+	if(!intervalRef) setInterval('drawHero()',500);
 }
 
 function getRandomInt(min, max) {
