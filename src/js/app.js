9d88c6820d308c3c551ef32bf3f1ae9d7d5a1fe8
hek4te <hek4te@gmail.com>
2018-03-25 01:04:21 +0100

    added features

--- src/js/app.js
+++ src/js/app.js
@@ -1,23 +1,35 @@
 var canvas = document.getElementById('game');
 canvas.style.backgroundColor = 'antiquewhite';
+canvas.style.border = '10px solid #0a0a0a';
 var ctx = canvas.getContext('2d');
+
 var mainHero = new Image();
-mainHero.addEventListener('load', setup, false);
 mainHero.src = 'hero_sprite.png';
+
 var intervalRef, 
+	score = 0,
 	enemiesNumber = 10,
 	heroSliceX = 0,
 	heroSliceY = 0,
 	heroPositionX = 522,
-	heroPositionY = 650;
+	heroPositionY = 650,
+	livebar = {
+		live: 100,
+		positionX: 500,
+		positionY: 750
+	};
 
-function setup() {
+//eslint-disable-next-line no-unused-vars
+function init(){
 	/*
 		TODO:
 		- licznik punktów (+ zapis w localstorage)
 		- ekran startowy gry (+ komiks przedstawiający fabułę)
 	*/
+	setup();
+}
 
+function setup() {
 	drawHero();
 	startHeroAnimation();
 	drawEnemies(enemiesNumber);
@@ -34,6 +46,16 @@ function drawHero(){
 		- możliwość śmierci bohatera - roślina usycha. Roślina potrzebuję jedzenia żeby nie uschnąć. W miarę zwiększenia poziomu roślina potrzebuje więcej jedzenia i szybciej usycha.
 		- poziom życia bohatera (poziom wyschnięcia rośliny) - jako symboliczna "doniczka" w której zmniejsza się np. poziom wody.
 	*/
+	ctx.font = '20px monospace';
+	ctx.fillText('Score: '+score,livebar.positionX + 130,livebar.positionY + 15);
+
+	ctx.font = '20px monospace';
+	ctx.fillText('Live:',livebar.positionX - 70,livebar.positionY + 15);
+	if( livebar.live > 0 ){
+		for(var j = 1; j <= livebar.live; j++) {
+			drawLiveBar(j);
+		}
+	}
 
 	ctx.clearRect(515,650,100,100);
 
@@ -57,11 +79,14 @@ function drawEnemies(enemiesNumber) {
 
 	var spacing = 48,
 		xOffset = 150,
-		speed = 2,
+		speed = 0.1,
 		direction = 1,
-		posX, posY;
-
-	enemies = [];
+		enemy = {
+			posX: 0, 
+			posY: 0,
+			vy: 1,
+		},
+		enemies = [];
 	for (var i = 0; i <= enemiesNumber; i++) {
 		// var enemy = g.rectangle(32, 32, 'red');
 		// var x = spacing * i + xOffset;
@@ -73,16 +98,35 @@ function drawEnemies(enemiesNumber) {
 		// direction *= -1;
 		// enemies.push(enemy);
 		// gameScene.addChild(enemy);
-		posX = getRandomInt(0, canvas.width - 15);
-		posY = getRandomInt(0, canvas.height - 200 - 15);
+		// posX = getRandomInt(-10, canvas.width);
+		direction = 1;
+		enemy.posX = 0;
+		enemy.posY = getRandomInt(-10, canvas.height - 200);
+		enemy.vy = speed * direction;
+		enemies.push(enemy);
+		enemy.posX += speed ;
+		enemy.posX = getRandomInt(-10, canvas.height - 200);
+		ctx.clearRect(enemy.posX - 20, enemy.posY, 15, 15);
 		ctx.fillStyle = '#a1a1a1';
-		ctx.fillRect(posX, posY, 15, 15);
+		ctx.fillRect(enemy.posX, enemy.posY, 15, 15);
 	}
 }
 
-function startHeroAnimation() {
-	if(!intervalRef) setInterval('drawHero()',500);
+// function animateEnemies(){
+// 	window.requestAnimationFrame(drawEnemies);
+// };
+
+function drawLiveBar(livePercent) {
+	ctx.fillStyle = '#000099';
+	ctx.fillRect(livebar.positionX+livePercent,livebar.positionY,1,20);
 }
+
+function startHeroAnimation() {
+	if(!intervalRef) {
+		setInterval('drawHero()',500);
+	}
+}
+
 
 function getRandomInt(min, max) {
 	return Math.floor(Math.random() * (max - min + 1)) + min;
